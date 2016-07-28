9ea9b1b2c04b70afe0f2028c85e0df83f44b1e5b
RohiniMaliCDK <rohinimali1994@gmail.com>
2016-07-28 12:40:40 +0530

    Updated card class

--- Card.java
+++ Card.java
@@ -1,65 +1,63 @@
-
+package cardsGame;
 
 /**
- * Created by phadkep on 7/28/2016.
+ * Created by malir on 7/28/2016.
  */
-
 public class Card {
-
-    int rank;
+    int pip;
     int suit;
 
     private static String[] suits = { "HEARTS", "SPADES", "DIAMONDS", "CLUBS" };
-    private static String[] ranks  = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
-
-
-    public static String rankAsString( int __rank ) {
-
-        return ranks[__rank];
-
-    }
+    private static String[] pips  = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
 
 
 
-    Card(short suit, short rank)
-
-    {
-
-        this.rank=rank;
-
+    Card(int suit, int pip) {
+        this.pip=pip-1;
         this.suit=suit;
+    }
 
+
+    public String toString() {
+        return pips[pip] + " of " + suits[suit];
     }
 
 
 
-    public String toString()
+    public int getPip() {
 
-    {
-
-        return ranks[rank] + " of " + suits[suit];
-
+        return pip+1;
     }
-
-
-
-    public int getRank() {
-
-        return rank;
-
-    }
-
 
 
     public int getSuit() {
 
         return suit;
-
     }
 
+    public int comparePip(Card a){
+        if(this.pip==a.pip){
+            return 0;
+        } else if(this.pip>a.pip){
+            return 1;
+        } else {
+            return -1;
+        }
+    }
 
+    public int compareSuit(Card a){
+        if(this.suit==a.suit){
+            return 1;
+        } else {
+            return 0;
+        }
+    }
 
-
+    public int compareCard(Card a){
+        if((this.pip==a.pip) && (this.suit==a.suit)){
+            return 1;
+        } else {
+            return 0;
+        }
+    }
 }
-
-
